6d015fdd7e482499d1815bd6d0cb593239d994ce
aethiopicuschan <aethiopicuschan@protonmail.com>
2022-11-03 11:02:27 +0900

    add test

--- repubcal_test.go
+++ repubcal_test.go
@@ -34,4 +34,30 @@ func TestNewDate(t *testing.T) {
 	if date.Weekday != "Nonidi" {
 		t.Errorf("want \"Nonidi\", got %s", date.Weekday)
 	}
+	// 18 Brumaire
+	date, err = repubcal.NewDate(time.Date(1799, 11, 9, 0, 0, 0, 0, time.UTC))
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if date.Year != "VIII" {
+		t.Errorf("want \"VII\", got %s", date.Year)
+	}
+	if date.YearInt != 8 {
+		t.Errorf("want 8, got %d", date.YearInt)
+	}
+	if date.Month != "Brumaire" {
+		t.Errorf("want \"Brumaire\", got %s", date.Month)
+	}
+	if date.MonthInt != 2 {
+		t.Errorf("want 2, got %d", date.MonthInt)
+	}
+	if date.Day != "Dentelaire" {
+		t.Errorf("want \"Dentelaire\", got %s", date.Day)
+	}
+	if date.DayInt != 18 {
+		t.Errorf("want 18, got %d", date.DayInt)
+	}
+	if date.Weekday != "Octidi" {
+		t.Errorf("want \"Octidi\", got %s", date.Weekday)
+	}
 }
